2be31a7b22b364e9a7b1e3587178ed79a43f526d
Sofia Carballo <sofia.carballo@live.com>
2020-09-27 18:17:23 +0200

    Add style to gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,12 +1,14 @@
 <template>
-    <div v-if="results.length == 0">
+    <div class="gallery display">
+        <div v-if="results.length == 0">
         No images were found
-    </div>
-    <section v-else>
-        <div v-for="(result, key) in results" :key="key">
-            <img :src="result.links[0].href" />
         </div>
-    </section>
+        <section v-else>
+            <div v-for="(result, key) in results" :key="key">
+                <img :src="result.links[0].href" />
+            </div>
+        </section>
+    </div>
 </template>
 <script>
 export default {
@@ -19,3 +21,41 @@ export default {
     } 
 }
 </script>
+
+<style scoped scss>
+.gallery {
+	width: 640px;
+	margin: 0 auto;
+	padding: 5px;
+	background: #fff;
+	box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+}
+.gallery > div {
+	position: relative;
+	float: left;
+	padding: 5px;
+	min-height: 220px;
+}
+.gallery > div > img {
+	width: 200px;
+	vertical-align: middle;
+	transition: 0.1s transform;
+	transform: translateZ(0); /* hack */
+}
+.gallery > div:hover {
+	z-index: 1;
+}
+.gallery > div:hover > img {
+	transform: scale(1.8, 1.8);
+	transition: 0.3s transform;
+}
+.display:before,
+.display:after {
+	display: table;
+	content: "";
+	line-height: 0;
+}
+.display:after {
+	clear: both;
+}
+</style>
